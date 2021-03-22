1b58289a2c50d14ee85fc5210a27b8c7505d987d
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-22 04:48:56 -0300

    Resolvido o exercíco 9 e os exercícios bônus

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4_2.js
@@ -75,3 +75,53 @@ for (let i = 1; i <= 25; i += 1) {
   arrayNumbers.push(i);
 }
 console.log(arrayNumbers);
+
+// 9 -Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+for (let index = 0; index < arrayNumbers.length; index += 1) {
+  console.log('O resultado da divisão de cada item do array anterior por 2 é: ' + arrayNumbers[index] / 2);
+}
+
+// Exercício bônus - 1 Ordene o array numbers em ordem crescente e imprima seus valores;
+
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let j = 0; j < i; j += 1) {
+    if (numbers[i] < numbers[j]) {
+      let posicao = numbers[i];
+
+      numbers[i] = numbers[j];
+      numbers[j] = posicao;
+    }
+  }
+}
+
+console.log(numbers);
+
+// Exercício bônus 2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let j = 0; j < i; j += 1) {
+    if (numbers[i] > numbers[j]) {
+      let posicao = numbers[i];
+
+      numbers[i] = numbers[j];
+      numbers[j] = posicao;
+    }
+  }
+}
+console.log(numbers);
+
+// Exercício bônus 3 - Agora você irá criar um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (valor correspondente) e 9 (próximo valor). Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push .
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novoArray = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i + 1]) {
+    novoArray.push(numbers[i] * numbers[i + 1]);
+
+  } else {
+    novoArray.push(numbers[i] * 2);
+  }
+}
+
+console.log(novoArray);
